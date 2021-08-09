9ef0fd2db1b52cfc02742d366b51ff7a990c07bf
palaktiwari21 <67467227+palaktiwari21@users.noreply.github.com>
2021-08-09 15:48:49 +0530

    Add files via upload

---
+++ 105.py
@@ -0,0 +1,18 @@
+import csv
+
+with open ('class1.csv',newline='')as f:
+    reader=csv.reader(f)
+    file_data=list(reader)
+file_data.pop(0)
+total_marks=0
+total_enteries=len(file_data)
+for marks in file_data:
+    total_marks+=float(marks[1])
+mean=total_marks/total_enteries
+print('Mean Is : '+str(mean))
+import plotly.express as px
+import pandas as pd
+df =pd.read_csv('class1.csv')
+fig=px.scatter(df,x='Student Number',y='Marks')
+fig.update_layout(shapes=[dict(type='line',y0=mean,y1=mean,x0=0,x1=total_enteries)])
+fig.show()
---
+++ graph2.py
@@ -0,0 +1,19 @@
+import csv
+
+with open ('class2.csv',newline='')as f:
+    reader=csv.reader(f)
+    file_data=list(reader)
+file_data.pop(0)
+total_marks=0
+total_enteries=len(file_data)
+for marks in file_data:
+    total_marks+=float(marks[1])
+mean=total_marks/total_enteries
+print('Mean Is : '+str(mean))
+import plotly.express as px
+import pandas as pd
+df =pd.read_csv('class2.csv')
+fig=px.scatter(df,x='Student Number',y='Marks')
+fig.update_layout(shapes=[dict(type='line',y0=mean,y1=mean,x0=0,x1=total_enteries)])
+fig.update_yaxes(rangemode='tozero')
+fig.show()
